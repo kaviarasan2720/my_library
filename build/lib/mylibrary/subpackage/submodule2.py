3897903f6bd2b3c0b21ee106164bb3385004a167
kaviarasan2720 <arasansk54@gmail.com>
2024-02-27 17:29:25 +0530

    all

--- build/lib/mylibrary/subpackage/submodule2.py
+++ build/lib/mylibrary/subpackage/submodule2.py
@@ -1,6 +1,15 @@
-class MySubClass:
-    def __init__(self, name):
-        self.name = name
+class Array:
+    def __init__(self, a=[]):
+        self.a = a
+        self.c = 0
 
-    def sub_greet(self):
-        print("Hello from subpackage,", self.name)
+    def append(self, q):
+        self.a += [q]
+        self.c += 1
+        return self.a
+
+    def delete(self, index=None, value=None):
+        for i in range(self.c):
+            if index == i or value == self.a[i]:
+                del self.a[i]
+                return self.a
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaviproject',
-    version='0.1',
+    version='0.3',
     packages=find_packages(),
     author='Kavi',
     author_email='arasansk54@gmail.com',
